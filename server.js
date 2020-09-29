09426e8d4437e65c79ffd2af08122eeb2a816c5f
samundra <samundranepal001@gmail.com>
2020-09-29 18:14:55 +0545

    server-edit

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4:uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server,{
-    debug:true
+const peerServer = ExpressPeerServer(server, {
+  debug: true
 });
-app.set('view engine','ejs');
-app.use(express.static('public'));//hey server our public file is going to be here
-app.use('/peerjs',peerServer);
-app.get('/',(req,res)=>{
-    res.redirect(`/${uuidv4()}`); //goes to api for room id with unique room id 
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-//set a api for a room id
-app.get('/:room',(req,res)=>{
-    res.render('room',{ roomId: req.params.room })
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
 
-io.on('connection',socket =>{
-    socket.on('join-room',(roomId,userId)=>{
-       socket.join(roomId);
-       socket.to(roomId).broadcast.emit('user-connected',userId);
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
 
-
-
-
-// const port = process.env.PORT || 3030
-server.listen(3030,()=>{
-    console.log(`server is runnning`)
-});
+server.listen(process.env.PORT||3030)
